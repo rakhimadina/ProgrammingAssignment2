c01381c0a449b10594e248a050be0e540253affa
rakhimadina <104272288+rakhimadina@users.noreply.github.com>
2022-04-24 18:01:38 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
 ##To solve this problem use two functions called makeCacheMatrix and cacheSolve
-
+##The folowing matrix is used as an example:
+## |5 3 2|
+## |4 6 7|
+## |1 8 0|
 makeCacheMatrix <- function( matt = matrix() ) {
 j <- NULL #cache value at first contains nothing so it is Null
     set <- function( matrix ){
@@ -13,22 +16,23 @@ list(set = set, get = get,
     setInvs = setInvs,
     getInvs = getInvs)
 }
-
+##The cacheSolve function should retrieve the inverse from the cache if the inverse has already been calculated and the matrix has not changed
 cacheSolve <- function(x, ...) {
-    matt <- x$getInvs()
+    j <- x$getInvs()
     ##condition to check the existence of cached value
-    if(!is.null(matt) ) {
+    if(!is.null(j) ) {
             message("getting cach ed of invers matt")
-            return(matt)
+            return(j)
     }
     info <- x$getm()
     ##compute the inverse by using matrix multiplication
-    matt <- solve(info) %*% info
+    j <- solve(info) %*% info
+    ##set new inversed matrix to our matrix
     x$setInverse(matt)
     matt
 }
 ##test the matrix and problem
-matt <-makeCacheMatrix(matrix(c(5,4,1,3,6,8,2,7,0),ncol=3,nrow=3))
-cacheSolve(matt)
+mat <-makeCacheMatrix(matrix(c(5,4,1,3,6,8,2,7,0),ncol=3,nrow=3))
+cacheSolve(mat)
 x$getm()
 x$getInvs()
